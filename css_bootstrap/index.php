8eea6a67a7788d3c73244fae120ac94262fd69e5
Harik med <hamouda.form@gmail.com>
2017-07-18 12:25:30 +0100

    rappel css / html bootsrap

---
+++ css_bootstrap/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bootstrap theme</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown open">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>   
+<!-- fin menu -->
+<!-- debut image/ debut container -->
+<div class="container"><img src="images/s.png" alt="slider supemir" 
+style="margin-top:60px" class="img-responsive" 
+><h2 class="alert alert-danger centerText">nos services </h2>
+<div class="row"><div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div><div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div></div>
+
+<div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div><div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div><div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div><div class="col-md-4 col-sm-6">
+  <h2>formation</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit incidunt et deleniti ea, quo veritatis nesciunt. Sapiente corporis quidem eos beatae quaerat, nesciunt at. Repellendus incidunt molestias cupiditate excepturi enim.</p>
+
+
+</div>
+<div class="netoyer"></div>
+<h3 class="alert alert-danger centerText">nos partenaires</h3>
+
+<div class="col-md-10 bordure centerZone">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dicta non pariatur eligendi voluptatibus porro consectetur nisi autem necessitatibus expedita eius similique itaque voluptas assumenda fuga, nam iure quos nobis.</p>
+
+</div>
+</div>
+<!-- fin container -->
+
+<!-- fin image -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
